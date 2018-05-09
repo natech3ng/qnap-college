e52d445f727e6ff84584300d95e29dcdf4186244
Nate Cheng <qqnc@users.noreply.github.com>
2018-05-08 18:13:03 -0700

    college

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,36 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, ViewChild, ElementRef, AfterViewInit } from '@angular/core';
+import { CategoryService } from './_services/category.service';
+import { Category } from './_models/category';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'app';
+export class AppComponent implements OnInit, AfterViewInit {
+
+  @ViewChild('cElement', {read: ElementRef}) cElement: ElementRef;
+  categories: Category [] = [];
+  cGridWidth: Number = 0;
+
+  constructor(private _categoryService: CategoryService) {
+    this._categoryService.all().subscribe(
+      (categories: Category []) => {
+        this.categories = categories;
+        console.log(this.categories);
+        setTimeout( () => {
+          this.cGridWidth = this.cElement.nativeElement.offsetWidth;
+          console.log(this.cElement.nativeElement.offsetWidth);
+        }, 100);
+      },
+      (error) => {
+      }
+    );
+  }
+
+  ngOnInit() {}
+
+  ngAfterViewInit() {
+    
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,21 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { HttpClientModule } from '@angular/common/http';
+import { NgPipesModule } from 'ngx-pipes';
 
 import { AppComponent } from './app.component';
+import { CategoryService } from './_services/category.service';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HttpClientModule,
+    NgPipesModule
   ],
-  providers: [],
+  providers: [CategoryService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
