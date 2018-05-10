46f5596c282d34850f5e23e1d80ffe3d57b6aac0
Nate Cheng <qqnc@users.noreply.github.com>
2018-05-10 14:31:10 -0700

    add loading

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,29 @@
 import { Component, OnInit, ViewChild, ElementRef, AfterViewInit } from '@angular/core';
 import { CategoryService } from './_services/category.service';
 import { Category } from './_models/category';
+import { Router, NavigationStart, NavigationEnd, NavigationCancel } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements AfterViewInit {
+  loading: Boolean;
+  constructor(private router: Router) {
+    this.loading = true;
+  }
+  ngAfterViewInit() {
+    this.router.events
+        .subscribe((event) => {
+            if(event instanceof NavigationStart) {
+                this.loading = true;
+            } else if (
+                event instanceof NavigationEnd ||
+                event instanceof NavigationCancel
+                ) {
+                this.loading = false;
+            }
+        });
+}
 }
