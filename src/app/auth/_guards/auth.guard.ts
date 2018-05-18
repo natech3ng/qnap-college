5eb648c1439e1dc2e1db89ac7823bea315770760
Nate Cheng <qqnc@users.noreply.github.com>
2018-05-18 11:29:43 -0700

    some editing bug and delete methof

--- src/app/auth/_guards/auth.guard.ts
+++ src/app/auth/_guards/auth.guard.ts
@@ -27,7 +27,7 @@ export class AuthGuard implements CanActivate {
     const currentUser = JSON.parse(localStorage.getItem('currentUser'));
 
     console.log(currentUser);
-    return this._authService.verify().subscribe(
+    return this._authService.verify().map(
       (data) => {
         console.log('data');
         console.log(data);
