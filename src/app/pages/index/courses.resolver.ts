c1d7451fb83a8e5b04da791a9f618fd7b6a0fc31
qqnc <boo0330@gmail.com>
2018-09-18 17:43:28 -0700

    convert-to-angular-universal

--- src/app/pages/index/courses.resolver.ts
+++ src/app/pages/index/courses.resolver.ts
@@ -1,18 +1,27 @@
 import { CourseDoc } from './../../_models/document';
 import { Resolve, ActivatedRouteSnapshot, RouterStateSnapshot, Router } from '@angular/router';
+import { OnInit, PLATFORM_ID, Inject } from '@angular/core';
+import { isPlatformServer, isPlatformBrowser } from '@angular/common';
 import { Observable } from 'rxjs';
-import { Course } from '../../_models/course';
+// import { Course } from '../../_models/course';
 import { CourseService } from '../../_services/course.service';
 import { Injectable } from '@angular/core';
 
 @Injectable()
 export class CoursesResolver implements Resolve<CourseDoc> {
-  constructor(private _courseService: CourseService, private _router: Router) {}
+  constructor(
+    private _courseService: CourseService,
+    private _router: Router,
+    @Inject(PLATFORM_ID) private platformId: Object) {}
   resolve(
     route: ActivatedRouteSnapshot,
     state: RouterStateSnapshot
   ): Observable<CourseDoc> | Promise<CourseDoc> | CourseDoc {
-    const cs = localStorage.getItem('currentDisplay') || 'Latest';
+    let cs = 'Latest';
+    if (isPlatformBrowser(this.platformId)) {
+      cs = localStorage.getItem('currentDisplay') || 'Latest';
+    }
+    
     let cs_value;
     for (const option of this._courseService.options) {
       if (option['name'] === cs) {
