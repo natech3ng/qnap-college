f5c16d372f99d64609b8d357d41a9bc417ea1ffd
NateC <boo0330@gmail.com>
2019-02-07 12:04:41 -0800

    add comment

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -3,7 +3,7 @@ module.exports = {
     name      : 'goqnapcom',
     script    : 'dist/server.js',
     env: {
-      NODE_ENV: 'true'
+      NODE_ENV: 'production'
     },
     env_production: {
       NODE_ENV: 'production'
@@ -17,14 +17,31 @@ module.exports = {
       host: ['blog.qnap.com'],
       ref: 'origin/master',
       repo: 'git@github.com:qqnc/qnap-college.git',
-      path: '/var/www/qnapcollege',
-      'pre-setup': 'sudo rm -rf /var/www/qnapcollege/source',
+      path: '/var/www/qnapcollege/production',
+      'pre-setup': 'sudo rm -rf /var/www/qnapcollege/production/source',
       'post-setup': 'npm install --unsafe-perm',
       'pre-deploy-local' : '',
       'pre-deploy' : '',
       'post-deploy' : 'cp ~/environment/college/.env ./; sudo pm2 restart ecosystem.config.js --env production; sudo chown -R deploy:deploy node_modules',
       env: {
         NODE_ENV: 'production'
+      }
+    },
+    staging: {
+      winkey: '/c/Users/nate/.ssh/google_cloud_deploy_openSSH',
+      key: '~/.ssh/id_rsa_deploy_google_cloud',
+      user: 'deploy',
+      host: ['blog.qnap.com'],
+      ref: 'origin/master',
+      repo: 'git@github.com:qqnc/qnap-college.git',
+      path: '/var/www/qnapcollege/staging',
+      'pre-setup': 'sudo rm -rf /var/www/qnapcollege/staging/source',
+      'post-setup': 'npm install --unsafe-perm',
+      'pre-deploy-local' : '',
+      'pre-deploy' : '',
+      'post-deploy' : 'cp ~/environment/college/staging/.env ./; sudo pm2 restart ecosystem.config.js --env staging; sudo chown -R deploy:deploy node_modules',
+      env: {
+        NODE_ENV: 'staging'
       }
     }
   }
