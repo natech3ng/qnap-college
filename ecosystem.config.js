414b333959feef4d252b030029a2697e977c3f0e
Nate Cheng <boo0330@gmail.com>
2019-02-07 15:47:02 -0800

    add staging script

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -31,7 +31,7 @@ module.exports = {
       winkey: '/c/Users/nate/.ssh/google_cloud_deploy_openSSH',
       key: '~/.ssh/id_rsa_deploy_google_cloud',
       user: 'deploy',
-      host: ['staging-college.natecheng.me'],
+      host: ['staging-go.natecheng.me'],
       ref: 'origin/master',
       repo: 'git@github.com:qqnc/qnap-college.git',
       path: '/var/www/qnapcollege/staging',
